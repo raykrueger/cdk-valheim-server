67907ec73df8babfc03eb1ea7040b1143b2a7d12
Ray Krueger <raykrueger@gmail.com>
2023-03-24 02:33:09 +0000

    Converted to CDK Application

---
+++ README.md
@@ -0,0 +1,20 @@
+# CDK Valheim Server
+
+This repository serves as an example of deploying
+[Valheim](https://www.valheimgame.com/) on AWS using
+[cdk-game-server](https://github.com/raykrueger/cdk-game-server).
+
+## Deploying
+
+To deploy your own Valheim server on AWS simply clone this repo, make any
+changes you'd like, and run `npx cdk deploy`. This will deploy Vaheim to ECS
+Fargate Spot. It will automatically shutdown if idle. It can be scaled up in the
+ECS UI, via the AWS CLI, or by enabling the [Discord
+bot](https://github.com/raykrueger/cdk-game-server).
+
+## History
+
+This repository used to host a cdk-valheim-server CDK Library. That library was
+very expensive when running, and was very hardcoded to Valheim. All of the
+functionality has been extracted to cdk-game-server and improved to be as cheap
+as possible.
---
+++ bin/cdk-valheim-server.ts
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { GameServer } from '@raykrueger/cdk-game-server';
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import { Secret } from 'aws-cdk-lib/aws-secretsmanager';
+
+const SECRET_NAME = "ValheimServer"
+
+class GameStack extends Stack {
+  constructor(scope: Construct, id: string, props: StackProps) {
+    super(scope, id, props)
+
+    const serverPasswordSecret = new Secret(this, 'GeneratedServerPasswordSecret', {
+      secretName: SECRET_NAME,
+      generateSecretString: {
+        passwordLength: 8,
+      },
+    });
+  
+    new GameServer(this, "Valheim", {
+      gamePorts: [
+        {portNumber: 2456, protocol: ecs.Protocol.UDP},
+      ],
+      mountTarget: "/root/.config/unity3d/IronGate/Valheim",
+      image: ecs.ContainerImage.fromRegistry("raykrueger/valheim"),
+      containerSecrets: {
+        SERVER_PASSWORD: ecs.Secret.fromSecretsManager(serverPasswordSecret),
+      }
+    })
+  }
+}
+
+const app = new cdk.App();
+new GameStack(app, "Valheim", {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: 'us-east-2',
+  }
+})
